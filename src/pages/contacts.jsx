3f8c0fe00c303be39503cb3f251f8a3bd28719fd
Anndersonn <slobodjanik1997@gmail.com>
2020-11-29 21:26:53 +0100

    common

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -65,13 +65,13 @@ const Contacts = () => {
     
     const [data, setData] = useState({name: '', email: '', message: '', company: ''})
     
-
+    const handleChange = e => setData({ ...data, [e.target.name]: e.target.value })
 
     const handleSubmit = e => {
       fetch('/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({ 'form-name': 'contact', ...data}),
+            body: encode({ 'form-name': 'contact'}),
           })
             .then(() => alert('success')) // успех)
             .catch(error => alert(error));
@@ -86,7 +86,6 @@ const Contacts = () => {
     //             [name]: value
     //     })
     // }
-    const handleChange = e => setData({ ...data, [e.target.name]: e.target.value })
     return (
         <Box component='div'>
             <Grid container justify='center'>
