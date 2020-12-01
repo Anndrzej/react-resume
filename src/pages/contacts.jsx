9850a459211e22c77fbcf99670e3425e62ac869b
Anndersonn <slobodjanik1997@gmail.com>
2020-12-01 18:44:49 +0100

    added styles to contact component

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,21 +1,28 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { TextField, Typography, Button, Grid, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
+import styles from './contacts.module.css'
+
 
 const useStyles = makeStyles(theme => ({
-    form: {
+    contactContainer: {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
         position: 'absolute',
+        margin: '0 auto',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    form: {
         textAlign: 'center',
-        [theme.breakpoints.down('md')]: {
-            width: '70%'
-        },
-        [theme.breakpoints.down('sm')]: {
-            width: '100%'
-        }
+        // [theme.breakpoints.down('md')]: {
+        //     width: '70%'
+        // },
+        // [theme.breakpoints.down('sm')]: {
+        //     width: '100%'
+        // }
     },
     button: {
         marginTop: '1rem',
@@ -27,6 +34,16 @@ const useStyles = makeStyles(theme => ({
         textTransform: 'uppercase',
         color: 'white',
         textAlign: 'center'
+    },
+    mainText: {
+        textTransform: 'uppercase',
+        color: 'white',
+        fontSize: '1.5rem',
+        fontWeight: 'bold'
+    },
+    subText: {
+        textTransform: 'uppercase',
+        color: 'white',
     },
 
 }))
@@ -58,38 +75,66 @@ const encode = (data) => {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
         .join("&");
-  }
+}
 
 const Contacts = () => {
     const classes = useStyles()
-    
-    const [data, setData] = useState({name: '', email: '', message: '', company: ''})
-    
-    // const handleChange = e => setData({ ...data, [e.target.name]: e.target.value })
+
+    const [data, setData] = useState({ name: '', email: '', message: '', company: '' })
 
     const handleSubmit = e => {
-      fetch('/', {
+        fetch('/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({ 'form-name': 'contact', ...data}),
-          })
+            body: encode({ 'form-name': 'contact', ...data }),
+        })
             .then(() => alert('success')) // успех)
             .catch(error => alert(error));
-      e.preventDefault();
+        e.preventDefault();
 
     }
 
     const handleChange = (e) => {
-        const {name, value} = e.target
-            setData({
-                ...data,
-                [name]: value
+        const { name, value } = e.target
+        setData({
+            ...data,
+            [name]: value
         })
     }
     return (
         <Box component='div'>
-            <Grid container justify='center'>
-                <Box
+            <Grid container item lg={10} className={classes.contactContainer}>
+                <Grid item lg ={4}>
+                    <Box className={styles.contactBox}>
+                        <Box className={styles.circle}>
+                            <img />
+                        </Box>
+                        <Box>
+                            <Typography className={classes.mainText}>Call me</Typography>
+                            <Typography className={classes.subText}>577077387</Typography>
+                        </Box>
+                    </Box>
+                    <Box className={styles.contactBox}>
+                        <Box className={styles.circle}>
+                            <img />
+                        </Box>
+                        <Box>
+                            <Typography className={classes.mainText}>Text me</Typography>
+                            <Typography className={classes.subText}>slobodjanik1997@gmail.com</Typography>
+                        </Box>
+                    </Box>
+                    <Box className={styles.contactBox}>
+                        <Box className={styles.circle}>
+                            <img />
+                        </Box>
+                        <Box>
+                            <Typography className={classes.mainText}>Address</Typography>
+                            <Typography className={classes.subText}>Jaworska 5, Wroclaw</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid
+                item lg={6}
                     component='form'
                     name='contacts'
                     className={classes.form}
@@ -109,7 +154,7 @@ const Contacts = () => {
                         size='small'
                         type="text"
                         name="name"
-                        value={data.name} 
+                        value={data.name}
                         onChange={handleChange} />
 
                     <InputField
@@ -122,7 +167,7 @@ const Contacts = () => {
                         size='small'
                         type="email"
                         name="email"
-                        value={data.email} 
+                        value={data.email}
                         onChange={handleChange} />
 
                     <InputField
@@ -135,7 +180,7 @@ const Contacts = () => {
                         size='small'
                         type="text"
                         name="company"
-                        value={data.company} 
+                        value={data.company}
                         onChange={handleChange} />
 
                     <InputField
@@ -149,7 +194,7 @@ const Contacts = () => {
                         margin='dense'
                         size='small'
                         name="message"
-                        value={data.message} 
+                        value={data.message}
                         onChange={handleChange} />
 
                     <Button
@@ -160,7 +205,7 @@ const Contacts = () => {
                         type='submit'>
                         contact me
                     </Button>
-                </Box>
+                </Grid>
             </Grid>
         </Box>
     );
