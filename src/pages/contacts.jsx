7690d2161290b2f89b361ad82c9b7032113bcba8
Anndersonn <slobodjanik1997@gmail.com>
2020-12-01 20:52:18 +0100

    test

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -149,8 +149,8 @@ const Contacts = () => {
                         </Box>
                     </Box>
                 </Grid>
-                <Grid
-                    item md={4} xs={9}
+                <Box
+                    // item md={4} xs={9}
                     component='form'
                     name='contacts'
                     className={classes.form}
@@ -221,7 +221,7 @@ const Contacts = () => {
                         type='submit'>
                         contact me
                     </Button>
-                </Grid>
+                </Box>
             </Grid>
         </Box>
     );
