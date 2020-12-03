d09cb0c453248dea0aa4f6fb0c417ddc6b470444
Anndersonn <slobodjanik1997@gmail.com>
2020-12-03 22:41:00 +0100

    restyling AGAIN resume component and added GASP animation in scroll

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,130 +1,101 @@
 import React from 'react';
-import { Typography, Box } from '@material-ui/core';
+import { Typography, Box, Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { gsap } from 'gsap';
+import { useEffect } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const useStyles = makeStyles(theme => ({
-    mainContainer: {
-        background: 'gray'
+    firstPage: { 
+         height: '100vh'
     },
-    timeLine: {
-        position: 'relative',
-        padding: '1rem',
-        margin: '0 auto',
-        '&:before': {
-            content: "''",
-            position: 'absolute',
-            height: '100%',
-            border: '1px solid tan',
-            right: '40px',
-            top: '0'
-        },
-        '&:after': {
-            content: "''",
-            display: 'table',
-            clear: 'both'
-        },
-        [theme.breakpoints.up('md')]: {
-            padding: '2rem',
-            '&:before': {
-                left: "50%",
-                right: 'auto'
-            }
-        }
-    },
-    timeLineItem: {
-        padding: '1rem',
-        borderBottom: '2px solid tan',
-        position: 'relative',
-        margin: '1rem 3rem 1rem 1rem',
-        clear: 'both',
-        '&:after': {
-            content: "''",
-            position: 'absolute'
-        },
-        '&:before': {
-            content: "''",
-            position: 'absolute',
-            right: '-0.625rem',
-            top: '50%',
-            borderStyle: 'solid',
-            borderColor: 'tomato tomato transparent transparent',
-            borderWidth: '0.625rem',
-            transform: 'rotate(45deg)'
-        },
-        [theme.breakpoints.up('md')]: {
-            width: '44%',
-            margin: '1rem',
-            '&:nth-of-type(2n)': {
-                float: 'right',
-                margin: '1rem',
-                borderColor: 'tan'
-            },
-            '&:nth-of-type(2n):before': {
-                right: 'auto',
-                left: '-0.625rem',
-                borderColor: 'transparent transparent tomato tomato'
-            }
-        }
-    },
-    timeLineYear: {
+    title: { 
         textAlign: 'center',
-        maxWidth: '9.375rem',
-        margin: '0 3rem 0 auto',
-        fontSize: '1.8rem',
-        background: 'tomato',
-        color: 'white',
-        lineHeight: 1,
-        padding: '0.5rem 0 1rem',
-        '&:before': {
-            display: 'none'
-        },
-        [theme.breakpoints.up('md')]: {
-            textAlign: 'center',
-            margin: '0 auto',
-            '&:nth-of-type(2n)': {
-                float: 'none',
-                margin: '0 auto',
-            },
-        }
+        marginTop: '3rem',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: '3rem'
     },
-    heading: {
-        color: 'tomato',
-        padding: '3rem 0',
-        textTransform: 'uppercase'
-    },
-    subHeading: {
-        color: 'white',
-        padding: '0',
-        textTransform: 'uppercase'
-    }
+    textWrapper: {
+        margin: '0 auto',
+        padding: '1rem',
+        backgroundColor: '#26262a',
+        boxShadow: '0px 0px 20px 3px rgba(0,0,0,0.36)',
+        marginBottom: '15rem'
+      },
+      company: { 
+          color: 'rgb(249, 108, 119)',
+          textTransform: 'capitalize',
+            textAlign: 'center'
+      },
+      text: { 
+          color: '#fff',
+          textAlign: 'center',
+          padding: '0.5rem'
+      },
+      proffesion: { 
+          textTransform: 'uppercase',
+          color: 'white',
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          textAlign: 'center'
+      },
+      year: { 
+          fontSize: '8rem',
+          fontWeight: 'bold',
+          color: '#2b2c2f', 
+      }
 }));
 
 function Resume() {
     const classes = useStyles();
+
+    // GSAP
+
+    useEffect(( ) => { 
+        gsap.from('.test', { 
+            duration: 3,
+            y: '100',
+            opacity: 0,
+            ease: 'ease-in',
+            scrollTrigger: {
+                trigger: '.year',
+                start: 'top 90%',
+                end: 'bottom 60%',
+                markers: true,
+                toggleActions: 'restart complete reverse reset'
+            }
+        })
+    }, []);
+
     return (
-        <Box component='header' className={classes.mainContainer}>
-            <Typography variant='h4' align='center' className={classes.heading}>Working experience</Typography>
-            <Box component='div' className={classes.timeLine}>
-                <Typography variant='h4' className={`${classes.timeLineYear} ${classes.timeLineItem}`}> 2017 </Typography>
-                <Box component='div' className={classes.timeLineItem}>
-                    <Typography variant="h5" align='center' className={classes.subHeading}>web design</Typography>
-                    <Typography variant="body1" align='center' style={{ color: 'tomato' }}>company name where worked</Typography>
-                    <Typography variant="subtitle1" align='center' style={{ color: 'tan' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, corrupti.</Typography>
-                </Box>
-                <Typography variant='h4' className={`${classes.timeLineYear} ${classes.timeLineItem}`}> 2015 </Typography>
-                <Box component='div' className={classes.timeLineItem}>
-                    <Typography variant="h5" align='center' className={classes.subHeading}>web design</Typography>
-                    <Typography variant="body1" align='center' style={{ color: 'tomato' }}>company name where worked</Typography>
-                    <Typography variant="subtitle1" align='center' style={{ color: 'tan' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, corrupti.</Typography>
-                </Box>
-                <Typography variant='h4' className={`${classes.timeLineYear} ${classes.timeLineItem}`}> 2013 </Typography>
-                <Box component='div' className={classes.timeLineItem}>
-                    <Typography variant="h5" align='center' className={classes.subHeading}>web design</Typography>
-                    <Typography variant="body1" align='center' style={{ color: 'tomato' }}>company name where worked</Typography>
-                    <Typography variant="subtitle1" align='center' style={{ color: 'tan' }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, corrupti.</Typography>
-                </Box>
+        <Container>
+            <Box className={classes.firstPage}> 
+                <Typography className={classes.title}> Working Experience</Typography>
             </Box>
-        </Box>
+            <Box component='div'>
+            <Typography className={`year ${classes.year}`}> 2017 </Typography>
+                <Grid item md={8} className={`test ${classes.textWrapper}`}>
+                    <Typography className={classes.proffesion}>web design</Typography>
+                    <Typography className={classes.company}>company name where worked</Typography>
+                    <Typography className={classes.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae distinctio, nisi illo recusandae debitis perspiciatis sapiente adipisci eligendi nostrum dolorem provident veniam quia doloremque possimus. Quam rerum labore voluptas dolorem?Incidunt sequi nulla repudiandae qui voluptatibus, dicta nemo rem quae delectus eius rerum. Laudantium voluptatibus perferendis praesentium similique, est libero necessitatibus corporis ea, rem tenetur ipsa cum provident! Eaque, culpa.</Typography>
+                </Grid>
+                <Typography className={classes.year}> 2015 </Typography>
+                <Grid item md={8} className={classes.textWrapper}>
+                    <Typography className={classes.proffesion}>web design</Typography>
+                    <Typography className={classes.company}>company name where worked</Typography>
+                    <Typography className={classes.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente praesentium tempora id! Eligendi eaque repellat, repudiandae placeat dolores asperiores quasi laboriosam veniam necessitatibus dolore ratione aspernatur fugiat incidunt facilis reiciendis.Aspernatur velit adipisci at quisquam veniam molestias architecto, sapiente praesentium suscipit hic optio voluptate nisi id aperiam animi laborum aliquid, cumque dolor molestiae, totam quae. Suscipit harum ipsam voluptatum fuga.</Typography>
+                </Grid>
+                <Typography className={classes.year}> 2013 </Typography>
+                <Grid item md={8} component='div' className={classes.textWrapper}>
+                    <Typography className={classes.proffesion}>web design</Typography>
+                    <Typography className={classes.company}>company name where worked</Typography>
+                    <Typography className={classes.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis ipsa quos laboriosam perferendis, similique inventore, dolor quasi iure, nesciunt tenetur assumenda laborum animi debitis asperiores sint quod velit cupiditate aperiam.Facilis esse temporibus, asperiores culpa perferendis beatae! Accusamus, obcaecati? Distinctio soluta harum veniam ratione possimus natus in aliquam quis. Nisi debitis eum perferendis nemo praesentium consectetur harum voluptatibus at eos!</Typography>
+                </Grid>
+            </Box>
+        </Container>
 
     )
 }
